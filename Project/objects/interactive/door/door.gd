53c6b85f942f37f82c183012ae906d9f37ac0665
Warlord56x <danika200151@gmail.com>
2025-05-31 03:30:20 +0200

    Added doors, and levers

---
+++ Project/objects/interactive/door/door.gd
@@ -0,0 +1,51 @@
+extends Node2D
+class_name Door
+
+## The node that is moved using tweens.
+@onready var door_body: AnimatableBody2D = $DoorBody
+
+
+## How much time it takes to open.
+@export var open_time: float = 0.2
+
+## How high the door should go.
+@export var open_amount: float = 0.5
+
+## How activation does it take to open .
+@export var opening_count: int = 1
+var current_opening: int = 0
+
+var is_open: bool = false
+
+## Handles opening the door.
+func open() -> void:
+	if is_open:
+		return
+	current_opening += 1
+	if current_opening >= opening_count:
+		is_open = true
+		create_tween().tween_property(
+			door_body,
+			"global_position:y",
+			global_position.y - open_amount,
+			open_time
+			)
+
+## Handles closing the door.
+func close() -> void:
+	if not is_open:
+		return
+	is_open = false
+	create_tween().tween_property(
+		door_body,
+		"global_position:y",
+		global_position.y + open_amount,
+		open_time
+		)
+
+## Handles the door in a flip-flop manner.
+func switch() -> void:
+	if is_open:
+		close()
+	else:
+		open()
